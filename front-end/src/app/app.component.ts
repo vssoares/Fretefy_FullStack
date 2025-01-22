a5f649d9822c09bfef0d8092f3c6e72945c302f2
vssoares <vsss23@hotmail.com>
2025-01-22 20:44:29 -0300

    angular 16

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { routeAnimation } from 'src/core/animations';
 
@@ -13,5 +13,14 @@ export class AppComponent {
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet?.activatedRouteData?.['animation'];
+  }
+
+  @HostListener('input', ['$event.target'])
+  onInputChange(target: HTMLInputElement): void {
+    if (target.value) {
+      target.classList.add('has-value');
+    }else {
+      target.classList.remove('has-value');
+    }
   }
 }
